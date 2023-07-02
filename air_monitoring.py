ee3575d1095b19ecaae8467362228f5331b15957
hiroyukigh <h.hiroyuki0123+github@gmail.com>
2023-07-02 23:13:22 +0900

    ignore追加、仮想環境作成、

---
+++ air_monitoring.py
@@ -0,0 +1,11 @@
+import time
+import serial
+
+ser = serial.Serial('/dev/serial0', 9600, timeout=1)
+
+while True:
+    result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
+    s=ser.read(9)
+    if s[0] == 0xff and s[1] == 0x86:
+        print ("CO2 = " + str(s[2]*256 + s[3]))
+    time.sleep(1)
